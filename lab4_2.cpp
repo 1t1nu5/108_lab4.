87077d1396a5d5628dce0ee632947307f051c224
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-01 16:51:06 +0700

    add cases

--- lab4_2.cpp
+++ lab4_2.cpp
@@ -1,11 +1,22 @@
 #include <stdio.h>
 int main()
 {
-	int stack, *pstack = &stack, current, time;
+	int stack, *pstack = &stack, current, time, receive;
 	scanf("%d", &time);
 	while (time > 0)
 	{
-		
+		scanf("%d", receive);
+		switch (receive)
+		{
+			case '+':
+				break;
+			case '-':
+				break;
+			case '*':
+				break;
+			case '/':
+				break;
+		}
 		time--;
 	}
 	return 0;

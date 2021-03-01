9299a30def6b613ef5c546a1724962acc6099ffc
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-01 18:52:26 +0700

    fixing codes...

--- lab4_1.cpp
+++ lab4_1.cpp
@@ -1,18 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 struct
 {
-	int total;
-	int current;
+	public:
+		int total;
+		int current;
 } year;
 int main()
 {
 	scanf("%d", year.total);
 	year.current = 0;
-	char receive[year.total];
+	char receive[100];
 	int medal[3][year.total], score;
 	for (int i = 0; i < year.total; i++)
 	{
-		
+		scanf("%s", receive);
 	}
 	for (int i = 0; i < year.total-3; i++)
 	{

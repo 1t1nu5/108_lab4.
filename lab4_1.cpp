7f4148150b0b8886dec4f4e9ccf19577037c220d
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-01 18:57:14 +0700

    turn alphabetic value into numeric value

--- lab4_1.cpp
+++ lab4_1.cpp
@@ -5,11 +5,13 @@ int main()
 	int ytotal, ycurrent;
 	scanf("%d", ytotal);
 	char receive[10], cut[3];
-	int medal[3][ytotal], score;
+	int medal[3][ytotal] = {}, score;
 	for (int i = 0; i < ytotal; i++)
 	{
 		scanf("%s", receive);
-		
+		medal[0][i] = receive[0] - '0';
+		medal[1][i] = receive[3] - '0';
+		medal[2][i] = receive[5] = '0';
 	}
 	for (int i = 0; i < ytotal-3; i++)
 	{

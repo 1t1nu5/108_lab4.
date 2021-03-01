9af018088597e048485de7e1534b79e77b1c641f
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-01 19:22:33 +0700

    fixing codes

--- lab4_4.cpp
+++ lab4_4.cpp
@@ -38,62 +38,57 @@ int main()
 		else if (m[0] == 'a' && m[1] == 'n' && m[2] == ' ')
 		{
 			cont = true;
-			first = false;
-			i++;
+			i += 2;
 		}
 		else if (m[0] == 'a' && m[1] == 'n' && m[2] == 'd' && m[3] == ' ')
 		{
 			cont = true;
-			first = false;
-			i++;
+			i += 3;
 		}
 		else if (m[0] == 'a' && m[1] == 't' && m[2] == ' ')
 		{
 			cont = true;
-			first = false;
-			i++;
+			i += 2;
 		}
 		else if (m[0] == 'f' && m[1] == 'o' && m[2] == 'r' && m[3] == ' ')
 		{
 			cont = true;
-			first = false;
-			i++;
+			i += 3;
 		}
 		else if (m[0] == 'i' && m[3] == ' ')
 		{
 			cont = true;
-			first = false;
 			i++;
 		}
 		else if (m[0] == 'i' && m[1] == 'n' && m[2] == ' ')
 		{
 			cont = true;
-			first = false;
-			i++;
+			i += 2;
 		}
 		else if (m[0] == 'o' && m[1] == 'f' && m[2] == ' ')
 		{
 			cont = true;
-			first = false;
-			i++;
+			i += 2;
 		}
 		else if (m[0] == 'o' && m[1] == 'n' && m[2] == ' ')
 		{
 			cont = true;
-			first = false;
-			i++;
+			i += 2;
 		}
 		else if (m[0] == 't' && m[1] == 'h' && m[2] == 'e' && m[3] == ' ')
 		{
 			cont = true;
-			first = false;
-			i++;
+			i += 3;
 		}
 		else if (m[0] == 'w' && m[1] == 'i' && m[2] == 't' && m[3] == 'h' && m[4] == ' ')
 		{
 			cont = true;
+			i += 4;
+		}
+		if (cont == true)
+		{
 			first = false;
-			i++;
+			continue;
 		}
 	}
 	return 0;

edb84330da783f38696c005f0c7ffcaa77c66a76
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-01 18:55:37 +0700

    delete struct

--- lab4_1.cpp
+++ lab4_1.cpp
@@ -1,22 +1,17 @@
 #include <stdio.h>
 #include <string.h>
-struct
-{
-	public:
-		int total;
-		int current;
-} year;
 int main()
 {
-	scanf("%d", year.total);
-	year.current = 0;
-	char receive[100];
-	int medal[3][year.total], score;
-	for (int i = 0; i < year.total; i++)
+	int ytotal, ycurrent;
+	scanf("%d", ytotal);
+	char receive[10], cut[3];
+	int medal[3][ytotal], score;
+	for (int i = 0; i < ytotal; i++)
 	{
 		scanf("%s", receive);
+		
 	}
-	for (int i = 0; i < year.total-3; i++)
+	for (int i = 0; i < ytotal-3; i++)
 	{
 		
 	}

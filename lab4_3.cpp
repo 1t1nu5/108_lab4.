21d40a22277bafe72cc1c25f6e8c9237c8dc809e
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-01 17:03:42 +0700

    add space ignoring code and DONE

--- lab4_3.cpp
+++ lab4_3.cpp
@@ -2,7 +2,7 @@
 #include <string.h>
 int main()
 {
-	char print[200] = {}, result[200];
+	char print[200] = {}, result[200] = {};
 	scanf("%s", print);
 	for (int i = 199; i > 0; i--)
 	{
@@ -11,9 +11,19 @@ int main()
 			print[i] = ' ';
 		}
 	}
+	int letter = 0;
 	for (int i = 0; i < 200; i++)
 	{
-		
+		if (print[i] == ' ')
+		{
+			continue;
+		}
+		else
+		{
+			result[letter] = print[i];
+			letter++;
+		}
 	}
+	printf("%s", result);
 	return 0;
 }

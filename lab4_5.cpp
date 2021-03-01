369bb6a90ae9fe7fc3262585c42a0abf53364b69
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-01 17:46:06 +0700

    add more library

--- lab4_5.cpp
+++ lab4_5.cpp
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 int main()
 {
 	unsigned int round;
-	unsigned short int locate;
+	unsigned short int locate = 0;
 	char swap[50];
 	scanf("%d", &round);
 	while (round > 0)
 	{
+		strcpy(swap, " ");
 		round--;
 	}
 	return 0;

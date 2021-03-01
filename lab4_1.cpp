9b7571c294935f546d2576100d1fd51bb3db397b
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-01 18:50:18 +0700

    add more for loop and variables

--- lab4_1.cpp
+++ lab4_1.cpp
@@ -8,8 +8,12 @@ int main()
 {
 	scanf("%d", year.total);
 	year.current = 0;
-	char receive;
-	int medal[3][3], score;
+	char receive[year.total];
+	int medal[3][year.total], score;
+	for (int i = 0; i < year.total; i++)
+	{
+		
+	}
 	for (int i = 0; i < year.total-3; i++)
 	{
 		

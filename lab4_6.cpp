c1cf3feb8919b2a727014744b2d90a6dab7f82ab
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-01 18:06:53 +0700

    add variable and input

--- lab4_6.cpp
+++ lab4_6.cpp
@@ -2,6 +2,11 @@
 #include <string.h>
 int main()
 {
+	char msg1[50], msg2[50], msg3[50], msg4[50];
+	scanf("%s", msg1);
+	scanf("%s", msg2);
+	scanf("%s", msg3);
+	scanf("%s", msg4);
 	
 	return 0;
 }

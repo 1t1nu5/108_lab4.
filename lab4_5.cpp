af64f6c1a9399d17cfc492997bd15b4c986b56f7
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-01 17:59:12 +0700

    almost done

--- lab4_5.cpp
+++ lab4_5.cpp
@@ -3,74 +3,74 @@
 int main()
 {
 	unsigned int round;
-	unsigned short int locate = 0;
+	unsigned short int locate = 1;
 	char swap[50];
 	scanf("%d", &round);
 	while (round > 0)
 	{
 		strcpy(swap, " ");
 		scanf("%s", swap);
-		for (int i = 0; i < 50 || swap[i] = ' '; i++)
+		for (int i = 0; i < 50 || swap[i] == ' '; i++)
 		{
 			switch (locate)
 			{
-				case 0:
-					switch (swap[i])
-					{
-						case 'D':
-							locate = 1;
-							break;
-						case 'C':
-							locate = 2;
-							break;
-						case 'B':
-						case 'E':
-							locate = 3;
-							break;
-					}
-					break;
 				case 1:
 					switch (swap[i])
 					{
 						case 'D':
-							locate = 0;
-							break;
-						case 'A':
-						case 'E':
 							locate = 2;
 							break;
 						case 'C':
 							locate = 3;
+							break;
+						case 'B':
+						case 'E':
+							locate = 4;
 							break;
 					}
 					break;
 				case 2:
 					switch (swap[i])
 					{
-						case 'C':
-							locate = 0;
+						case 'D':
+							locate = 1;
 							break;
 						case 'A':
 						case 'E':
-							locate = 1;
-							break;
-						case 'D':
 							locate = 3;
+							break;
+						case 'C':
+							locate = 4;
 							break;
 					}
 					break;
 				case 3:
 					switch (swap[i])
 					{
-						case 'B':
-						case 'E':
-							locate = 0;
-							break;
 						case 'C':
 							locate = 1;
 							break;
+						case 'A':
+						case 'E':
+							locate = 3;
+							break;
 						case 'D':
+							locate = 4;
+							break;
+					}
+					break;
+				case 4:
+					switch (swap[i])
+					{
+						case 'B':
+						case 'E':
+							locate = 1;
+							break;
+						case 'C':
 							locate = 2;
+							break;
+						case 'D':
+							locate = 3;
 							break;
 					}
 					break;
@@ -78,5 +78,6 @@ int main()
 		}
 		round--;
 	}
+	printf("%d", locate);
 	return 0;
 }

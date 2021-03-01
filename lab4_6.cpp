ec23a198726ff05cfd7282d102553f069c30e26c
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-01 18:09:56 +0700

    add De capitalize function

--- lab4_6.cpp
+++ lab4_6.cpp
@@ -1,12 +1,25 @@
 #include <stdio.h>
 #include <string.h>
+void decap(char msg[])
+{
+	for (int i = 0; i < 50; i++)
+	{
+		if (msg[i] >= 'A' && msg[i] <= 'Z')
+		{
+			msg[i] - 'A' + 'a';
+		}
+	}
+}
 int main()
 {
 	char msg1[50], msg2[50], msg3[50], msg4[50];
 	scanf("%s", msg1);
+	decap(msg1);
 	scanf("%s", msg2);
+	decap(msg2);
 	scanf("%s", msg3);
+	decap(msg3);
 	scanf("%s", msg4);
-	
+	decap(msg4);
 	return 0;
 }
